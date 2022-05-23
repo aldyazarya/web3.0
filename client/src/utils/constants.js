8cec58ba5bd8e81d730174af34f2b85f97b2b03d
Aldy Azarya <aldy1612@gmail.com>
2022-05-23 23:27:06 +0800

    fixing bugs on constant-new

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,4 @@
 import abi from "./Transactions.json";
 
 export const contractABI = abi.abi;
-// export const  contractAddress = [`${import.meta.env.VITE_CONTRACT_ADDRESS}`];
-export const contractAddress = "0xb272B687316eE3A42c85DFcC0a9CCaF1BE4EB702";
+export const  contractAddress = [`${import.meta.env.VITE_CONTRACT_ADDRESS}`];
